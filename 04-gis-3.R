d415d844165ed2d1ade243108c8f01822ee0cc02
Firat Gundem <firatgundem@gmail.com>
2019-02-05 13:12:05 -0600

    5th of February

---
+++ 04-gis-3.R
@@ -0,0 +1,58 @@
+# Load libraries for use
+setwd("~/R/workshops-scripts")
+library(sf)
+library(ggplot2)
+library(dplyr)
+
+# Read in libraries and areas data
+libraries <- st_read("https://data.cityofchicago.org/resource/psqp-6rmg.geojson")
+areas <- st_read("https://data.cityofchicago.org/resource/igwz-8jzy.geojson")
+
+
+# Project both
+libraries <- st_transform(libraries, 32616)
+areas <- st_transform(areas, 32616)
+
+# Make a ggplot with libraries and community areas
+ggplot() +
+  geom_sf(data=areas) +
+  geom_sf(data=libraries)
+
+# Find which areas intersects with libraries and save as a variable called "intersects"
+
+intersects <- st_intersects(areas, libraries) 
+intersects
+
+# Filter areas by *without* libraries. Save as a variable called "no_lib" Hint: use "==" instead if ">" in the logical comparison
+
+no_lib <- filter(areas, lengths(intersects)==0)
+
+ggplot() +
+  geom_sf(data=areas) +
+  geom_sf(data=no_lib, fill="pink")+
+  geom_sf(data = libraries)
+
+ggsave("doc/areas_without_libraries.png")
+
+
+ggsave("doc/areas_without_libraries_big.png", width = 4, height = 4)
+
+st_join(libraries)
+st_join(libraries, areas["community"])
+
+areas <- select(areas, community, area_numbe, geometry)
+
+lib_join <- st_join(libraries, areas)
+
+
+#this is just to help me figure out of which columns to select
+
+names(areas)
+str(areas)
+glimpse(areas)
+
+
+lib_counts <- count(lib_join, community)
+
+arrange(lib_counts, desc(n))
+
